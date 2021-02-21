3381b8f15ffc870cea4d2529ea89eb6556c01084
Hogjesty <64978483+Hogjesty@users.noreply.github.com>
2021-02-21 18:04:09 +0200

    added Multiplication and Division methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
         Complex c1 = new TComplex(1, Math.PI/2);
@@ -6,5 +9,7 @@ public class Main {
         System.out.println(c2);
         System.out.println(c1);
         System.out.println(Complex.sum(c2, c1));
+
+
     }
 }
